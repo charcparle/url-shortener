84afaab461ece0eda861c370d9f8740cbfd27acc
Charles Cheng <charles.yhcheng@gmail.com>
2020-11-23 08:25:49 +0000

    Added dns.lookup and regex to validate url

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
+const dns = require('dns');
 
 // Basic Configuration
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
+});
 
 // Request Logger
 app.use((req,res,next)=>{
@@ -29,6 +33,7 @@ mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology
 // Handle CORS
 app.use(cors());
 
+// Serve static files
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 // Landing page
@@ -41,11 +46,8 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
 
-// Set yp DB
+// Set up DB
 const Schema = mongoose.Schema;
 const urlSchema = new Schema({
   original: {type: String, required: true},
@@ -54,8 +56,8 @@ const urlSchema = new Schema({
 let Link = mongoose.model('Link', urlSchema);
 console.log(Link);
 
-// Mongoose fn to store URL
-const createShortURL = (original) => {
+// Mongoose fn to check whether URL is duplicated
+const findShortURL = (original) => {
   let query =Link.find({original: original},(err, linkFound)=>{
     console.log(`linkFound.length: ${linkFound.length}`);
     if (err) {
@@ -74,26 +76,43 @@ const createShortURL = (original) => {
 
 //https://stackoverflow.com/questions/24035872/return-results-mongoose-in-find-query-to-a-variable
 
-
-// Capture the POST URL action
+const regex=/^https*:\/\//;
+const string = "https://gmail.com/http://";
+console.log(string.match(regex));
+console.log(string.replace(regex,''))
+// Capture the POST URL action, create & save
 app.post("/api/shorturl/new",(req,res)=>{
-  let query = createShortURL(req.body.url);
-  query.exec((err,linkFound)=>{
-    if (err) return console.error(err);
-    if (linkFound.length==0){
-      console.log(`None found, linkFound inside query of app.post: ${linkFound}`);
-      let newLink = new Link({
-        original: req.body.url,
-        short: Math.random()
-      })
-      newLink.save((err=>{
-        if (err) return console.error(err);
-      }));
-      res.json({original_url: req.body.url, short_url: newLink.short});
+  let capture = req.body.url.replace(regex,'');
+  console.log(`capture: ${capture}`)
+  dns.lookup(capture,(err,address,family)=>{
+    if (err) {
+      console.log(err);
+      res.json({error: 'Invalid hostname'});
+    } else if (req.body.url.match(regex)==null) {
+      console.log(`req.body.url.match(regex): ${req.body.url.match(regex)}`);
+      console.log("url should starts with http or https");
+      res.json({error: 'Invalid url'});
     } else {
-      console.log(`Matched, linkFound inside query of app.post: ${linkFound}`);
-      console.log(`linkFound[0].short: ${linkFound[0].short}`);
-      res.json({original_url: req.body.url, short_url: linkFound[0].short});
+      console.log(`dns-address: ${address}`);
+      let query = findShortURL(req.body.url);
+      query.exec((err,linkFound)=>{
+        if (err) return console.error(err);
+        if (linkFound.length==0){
+          console.log(`None found, linkFound inside query of app.post: ${linkFound}`);
+          let newLink = new Link({
+            original: req.body.url,
+            short: Math.random()
+          })
+          newLink.save((err=>{
+            if (err) return console.error(err);
+          }));
+          res.json({original_url: req.body.url, short_url: newLink.short});
+        } else {
+          console.log(`Matched, linkFound inside query of app.post: ${linkFound}`);
+          console.log(`linkFound[0].short: ${linkFound[0].short}`);
+          res.json({original_url: req.body.url, short_url: linkFound[0].short});
+        }
+      });
     }
-  });
+  }); 
 });
